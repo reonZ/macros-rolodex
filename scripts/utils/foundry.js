5b30ceebaa02bdf0a1d71c916207ac3bdd803a83
Idle <651462+reonZ@users.noreply.github.com>
2022-11-15 14:58:42 +0100

    removed unused

--- scripts/utils/foundry.js
+++ scripts/utils/foundry.js
@@ -5,16 +5,6 @@ export function templatePath(...path) {
     return `modules/${MODULE_ID}/templates/${path.join('/')}`
 }
 
-/**
- * @param {string} key
- * @param {object} [data]
- */
-export function localize(key, data) {
-    key = `${MODULE_ID}.${key}`
-    if (data) return game.i18n.format(key, data)
-    return game.i18n.localize(key)
-}
-
 /**@param {string} key*/
 export function getSetting(key) {
     return game.settings.get(MODULE_ID, key)
