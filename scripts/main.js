631e8c7404dc9fb8753d9356f91b0b6d595cc543
Idle <651462+reonZ@users.noreply.github.com>
2022-11-15 15:02:09 +0100

    oopsy

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,12 @@
 import { setMacroFolder } from './folders.js'
 import { onCreateMacro } from './macro.js'
-import { registerSetting, templatePath } from './utils/foundry.js'
+import { registerSetting } from './utils/foundry.js'
 import { registerWrapper } from './utils/libwrapper.js'
 import { socketOn } from './utils/socket.js'
 
 const WRAPPER = 'Macro.createDocuments'
 
 Hooks.once('init', async () => {
-    await loadTemplates([templatePath('folder.html')])
-
     registerSetting({
         name: 'gamemaster',
         config: true,
--- scripts/utils/foundry.js
+++ scripts/utils/foundry.js
@@ -1,10 +1,5 @@
 import MODULE_ID from './module.js'
 
-/** @param {...string} path */
-export function templatePath(...path) {
-    return `modules/${MODULE_ID}/templates/${path.join('/')}`
-}
-
 /**@param {string} key*/
 export function getSetting(key) {
     return game.settings.get(MODULE_ID, key)
